498ef8c3e9565e51c21ff3dc3b05f8a4c91de96f
hellodit <asditap@gmail.com>
2021-03-26 15:18:17 +0700

    Add new example

--- example/main.go
+++ example/main.go
@@ -6,19 +6,51 @@ import (
 	"log"
 )
 
-func main(){
-	client := goks.NewClient(goks.NewCacheOptions().SetMaxSizeItem(3))
-	err := client.Set("key-1","value brot")
+func main() {
+	client := goks.NewClient(goks.NewCacheOptions().SetMaxSizeItem(5))
+	//insert cache key value
+	for i := 1; i <= 5; i++ {
+		err := client.Set(fmt.Sprintf("key-%d", i), fmt.Sprintf("value-%d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Get Cache key
+	keys, err := client.GetKeys()
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = client.Set("key-2","value brot")
-	if err != nil {
-		log.Fatal(err)
+
+	// Peek Cache by key
+	for _, key := range keys {
+		res, _ := client.PeekByKey(key)
+		fmt.Println("Cache value list =>", res)
 	}
+
+	//Get cache by key
 	val, err := client.Get("key-1")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(val)
+	fmt.Println("Value with given key =>", val)
+
+	// Insert new exceed capacity
+	err = client.Set("key-6", "value-6")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get new Cache key
+	newKeys, err := client.GetKeys()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Peek Cache by key
+	for _, key := range newKeys {
+		res, _ := client.PeekByKey(key)
+		fmt.Println("Cache value list =>", res)
+
+	}
 }
